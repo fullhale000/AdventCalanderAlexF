11f694f6611ff2aa77580a25ebdfffd1a1ff8061
fullhale000 <73959464+fullhale000@users.noreply.github.com>
2020-12-18 11:56:02 -0500

    Add files via upload

--- advent.js
+++ advent.js
@@ -25,3 +25,62 @@ const icons = [
     '&#x1F56F;',
     '&#x26c4;'
   ];
+const boxes = document.querySelectorAll('.num');
+
+//Function to control the click on the boxes
+
+function handelBoxClick(event){
+
+  const clickedBox = event.currentTarget;
+  const dayClicked = clickedBox.dataset.day;
+  const today = new Date();
+if(today.getDate() >= Number(dayClicked)){
+    clickedBox.innerHTML = icons[Number(dayClicked)]
+    storeClickedBoxes(Number(dayClicked));
+}
+else{
+console.log('no peeking!');
+}
+}
+
+boxes.forEach(function (box){
+  box.addEventListener('click', handelBoxClick)
+});
+
+
+
+let daysOpened = JSON.parse(localStorage.getItem('daysClicked'));
+
+
+
+function storeClickedBoxes(day){
+
+if (!localStorage.getItem('daysClicked')){
+  daysOpened = [];
+}else{
+  daysOpened = JSON.parse(localStorage.getItem('daysClicked'));
+}
+if (!daysOpened.includes(day)){
+
+daysOpened.push(day);}
+
+localStorage.setItem('daysClicked',JSON.stringify(daysOpened));
+}
+
+function randomizeIcons(oldList){
+  let randomList = [];
+
+  if(!localStorage.getItem('icons')){
+    while(oldList.length >0) {
+      const index = Math.floor(Math.random()*oldList.length);
+
+      randomList.push(oldList[index]);
+
+    oldList.splice(index,1);
+    }
+    localStorage.setItem('icons', JSON.stringify(ramdomList));
+  } else{
+    randomList = JSON.parse(localStorage.getItem('icons'));
+  }
+
+}
